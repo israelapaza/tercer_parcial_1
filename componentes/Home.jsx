8b2286c2588f316f95abc05d12b1b2ee43d6ea20
israelapaza <apaza.6924406@gmail.com>
2025-06-14 09:23:43 -0400

    componente home

---
+++ componentes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Inicio</h2>
+      <p>Bienvenido a la página principal.</p>
+    </section>
+  );
+};
+
+export default Home;
